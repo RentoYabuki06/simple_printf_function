fe5ab04de2cdb8ac101138dc3d21adb40da2097c
Rento Yabuki <ryabuki@c6r1s14.42tokyo.jp>
2025-04-25 20:16:21 +0900

    norm:ft_print_percent_bonus.c

--- bonus/srcs/print_functions/ft_print_percent_bonus.c
+++ bonus/srcs/print_functions/ft_print_percent_bonus.c
@@ -6,16 +6,14 @@
 /*   By: ryabuki <ryabuki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 14:38:52 by yabukirento       #+#    #+#             */
-/*   Updated: 2025/04/23 16:39:19 by ryabuki          ###   ########.fr       */
+/*   Updated: 2025/04/25 20:15:33 by ryabuki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf_bonus.h"
 
-int	ft_print_percent_bonus(t_option *option)
+int	ft_print_percent_bonus(void)
 {
-	(void)	option;
-
 	if (write(STDOUT_FILENO, "%", 1) < 0)
 		return (-1);
 	return (1);
