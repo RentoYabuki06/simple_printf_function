6f18408dee5c723fcdaf9f924b5a4dded3e60b2e
Rento Yabuki <ryabuki@c6r1s14.42tokyo.jp>
2025-04-25 20:13:45 +0900

    norm:ft_print_str_bonus.c

--- bonus/srcs/print_functions/ft_print_str_bonus.c
+++ bonus/srcs/print_functions/ft_print_str_bonus.c
@@ -5,45 +5,44 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ryabuki <ryabuki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/18 14:39:03 by yabukirento       #+#    #+#             */
-/*   Updated: 2025/04/23 12:27:38 by ryabuki          ###   ########.fr       */
+/*   Created: 2025/04/25 20:13:08 by ryabuki           #+#    #+#             */
+/*   Updated: 2025/04/25 20:13:10 by ryabuki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf_bonus.h"
 
+static int	ft_helper(char *str, int len_str, int len_space)
+{
+	if (write(STDOUT_FILENO, str, len_str) < 0)
+		return (EXIT_FAILURE);
+	if (ft_put_space(len_space) < 0)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
+
 int	ft_print_str_bonus(char *str, t_option *option)
 {
-	int	len_total;
 	int	len_str;
-	int	width;
-	int precision;
 	int	len_space;
 
 	if (!str)
 		return (write(STDOUT_FILENO, "(null)", 6));
-	precision = (*option).precision;
-	width = (*option).width;
 	len_str = ft_strlen(str);
-	if (precision != -1 && precision < len_str)
-		len_str = precision;
-	len_space = width - len_str;
+	if ((*option).precision != -1 && (*option).precision < len_str)
+		len_str = (*option).precision;
+	len_space = (*option).width - len_str;
 	if (len_space < 0)
 		len_space = 0;
-	len_total = len_str + len_space;
 	if ((*option).left_aligned == true)
 	{
-		if (write(STDOUT_FILENO, str, len_str) < 0)
-			return (-1);
-		if (ft_put_space(len_space) < 0)
+		if (ft_helper(str, len_str, len_space) == EXIT_FAILURE)
 			return (-1);
 	}
 	if ((*option).left_aligned == false)
 	{
-		if (ft_put_space(len_space) < 0)
-			return (-1);
-		if (write(STDOUT_FILENO, str, len_str) < 0)
+		if (ft_helper(str, len_str, len_space) == EXIT_FAILURE)
 			return (-1);
 	}
-	return (len_total);
+	return (len_str + len_space);
 }
